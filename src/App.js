e5b2b58a75ecc6dc4c5fee8d415cc4a5acdea686
Ash W <amsheppard2016@gmail.com>
2020-04-14 07:48:12 -0400

    mvp done

--- src/App.js
+++ src/App.js
@@ -1,79 +1,16 @@
-import React, { useState } from "react";
-import TodoForm from "./components/TodoForm";
+import React from "react";
+//import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 import "./components/Todo.css";
 
 function App() {
-    const [todoItems, setTodoItems] = useState([
-        {
-            task: "Organize Garage",
-            id: 1528817077241,
-            completed: false,
-        },
-        {
-            task: "Bake Cookies",
-            id: 1528817084342,
-            completed: false,
-        },
-        {
-            task: "Cook Dinner",
-            id: 1528817084343,
-            completed: false,
-        },
-        {
-            task: "Do Laundry",
-            id: 1528817084344,
-            completed: false,
-        },
-    ]);
-
-    const toggleCompleted = (itemId) => {
-        console.log("itemId", itemId);
-        setTodoItems(
-            todoItems.map((item) => {
-                if (item.id === itemId) {
-                    return {
-                        ...item,
-                        completed: !item.completed,
-                    };
-                }
-                return item;
-            })
-        );
-    };
-
-    const addItem = (itemTask) => {
-        setTodoItems([
-            ...todoItems,
-            {
-                task: itemTask,
-                id: Math.random(),
-                completed: false,
-            },
-        ]);
-    };
-
-    const clearCompleted = () => {
-        console.log("clearCompleted");
-        setTodoItems(
-            todoItems.filter((item) => {
-                return !item.completed;
-            })
-        );
-    };
-
     return (
         <div className="app">
             <div className="header">
                 <h1>To Do App!</h1>
-                <TodoForm addItem={addItem} />
             </div>
-            <TodoList
-                todoItems={todoItems}
-                toggleCompleted={toggleCompleted}
-                clearCompleted={clearCompleted}
-            />
+            <TodoList />
         </div>
     );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,20 +1,60 @@
-import React from "react";
-import Todo from "./Todo";
+import React, { useState, useReducer } from "react";
+import { todoReducer, initialTodoState } from "../reducers/todoReducer";
 
-const TodoList = (props) => {
-    console.log("todolist", props);
+const TodoList = () => {
+    const [todoState, dispatch] = useReducer(todoReducer, initialTodoState);
+    //console.log("todoState", todoState);
+    // const [todoItems, setTodoItems] = useState([]);
+    const [task, setTask] = useState("");
+
+    const handleChanges = (e) => {
+        setTask(e.target.value);
+    };
     return (
-        <div className="todo-list">
-            {props.todoItems.map((item) => (
-                <Todo
-                    key={item.id}
-                    item={item}
-                    toggleCompleted={props.toggleCompleted}
+        <div className="to-do-list">
+            <form>
+                <input
+                    type="text"
+                    name="newTask"
+                    value={task}
+                    onChange={handleChanges}
                 />
-            ))}
-            <button className="clear-btn" onClick={props.clearCompleted}>
-                Clear Completed
-            </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        dispatch({ type: "ADD_TODO", payload: task });
+                    }}
+                >
+                    Add
+                </button>
+            </form>
+            <div className="todo-list">
+                {todoState.todoItems.map((item) => {
+                    //console.log("item", item);
+                    return (
+                        <div
+                            key={item.id}
+                            className={`item${
+                                item.completed ? " completed" : ""
+                            }`}
+                            onClick={(e) =>
+                                dispatch({
+                                    type: "TOGGLE_COMPLETED",
+                                    payload: item.id,
+                                })
+                            }
+                        >
+                            <p>{item.task}</p>
+                        </div>
+                    );
+                })}
+                <button
+                    className="clear-btn"
+                    onClick={() => dispatch({ type: "CLEAR_COMPLETED" })}
+                >
+                    Clear Completed
+                </button>
+            </div>
         </div>
     );
 };
